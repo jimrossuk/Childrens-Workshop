fd43551908157f741adfb63eb77821960d16ce3e
Jim Ross <jimrossuk@gmail.com>
2020-04-08 16:41:41 +0100

    changed first pargaraph to larger text

--- front-page.php
+++ front-page.php
@@ -3,26 +3,29 @@ get_header();
 
 ?>
 
-<img src= " <?php the_field('main_image') ; ?> ">
 
-<p>  <?php  the_field('front_page_into1'); ?>  </p>
-<p>  <?php  the_field('front_page_info2'); ?>  </p>
-<p>  <?php  the_field('front_page_info3'); ?>  </p>
-<p>  <?php  the_field('front_page_info4'); ?>  </p>
-<p>  <?php  the_field('front_page_info5'); ?>  </p>
+    <img src= " <?php the_field('main_image') ; ?> ">
+    <div class="fp-graph">
+        <p class="fst-graph">  <?php  the_field('front_page_into1'); ?>  </p>
+        <p>  <?php  the_field('front_page_info2'); ?>  </p>
+        <p>  <?php  the_field('front_page_info3'); ?>  </p>
+        <p>  <?php  the_field('front_page_info4'); ?>  </p>
+        <p>  <?php  the_field('front_page_info5'); ?>  </p>
+    </div>
+    
 
 
 <?php 
 $link = get_field('ofsted');
 if( $link ): ?>
-    <a class="button" href="<?php echo esc_url( $link ); ?>">Ofsted report</a>
+    <a class="button ft-page" href="<?php echo esc_url( $link ); ?>">Ofsted report</a>
 <?php endif; ?>
 
 <div>
     <?php 
     $link = get_field('charity_link');
     if( $link ): ?>
-        <a class="button" href="<?php echo esc_url( $link ); ?>">Charity information</a>
+        <a class="button ft-page" href="<?php echo esc_url( $link ); ?>">Charity information</a>
     <?php endif; ?>
 </div>
 
