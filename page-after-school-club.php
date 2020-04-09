9df0a960581d177f78b19855f39ad83f86545baa
Jim Ross <jimrossuk@gmail.com>
2020-04-09 16:10:31 +0100

    added 2 custom fields for fees. Styled the content too

--- page-after-school-club.php
+++ page-after-school-club.php
@@ -3,11 +3,14 @@
 get_header();
 
 ?>
-<h2 class="text-center"> <?php the_field('after_school_info'); ?> </h2>
-<h3 class="text-center"> <?php the_field('routine'); ?>  </h3>
-<h3 class="text-center"> <?php the_field('routine_descript'); ?>  </h3>
-<h3 class="text-center"> <?php the_field('booking'); ?>  </h3>
-<h3 class="text-center"> <?php the_field('booking_desript'); ?>  </h3>
+<img src= " <?php the_field('asc_photo') ; ?> ">
+<h3 class="intro pad"> <?php the_field('after_school_club_intro'); ?> </h3>
+<h2 class="text-center pad"> <?php the_field('routine'); ?>  </h2>
+<p class="t pad"> <?php the_field('routine_descript'); ?>  </p>
+<h2 class="text-center pad"> <?php the_field('booking'); ?>  </h2>
+<p class="t pad"> <?php the_field('booking_descript'); ?>  </p>
+<p class="t pad"> Fee per child: £<?php the_field('fee_per_child'); ?>  </p>
+<p class="t pad"> Annual Fee: £<?php the_field('annual_fee'); ?>   </p>
 <?php
 get_footer();
 
