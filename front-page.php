3c59cd6027d76d5dba51c51d4ce204df2ecb29e8
Jim Ross <jimrossuk@gmail.com>
2020-04-12 17:27:16 +0100

    modified Location section on Front page. Now has Light-green background. Email/phone info. Links work

--- front-page.php
+++ front-page.php
@@ -12,39 +12,53 @@ get_header();
         <h2>  <?php  the_field('front_page_info4'); ?>  </h2>
         <p>  <?php  the_field('front_page_info5'); ?>  </p>
     </div>
-    
-<div>
-    <h1>Location</h1>
-    <p>Email</p>
-    <a href="<?php the_field('email_address', 'option'); ?>" > <?php the_field('email_address', 'option'); ?></a>
-    
-    <p>Mobile</p>
-    
-    <a href="tel:<?php the_field('mobile_number', 'option'); ?>"> <?php the_field('mobile_number', 'option');  ?></a>
-    <p> Landline</p>
-    
-    <a href="tel:<?php the_field('landline_number', 'option'); ?>"> <?php the_field('landline_number', 'option');  ?></a>
-<a href="https://www.google.com/maps/place/Totterdown+Children's+Community+Workshop/@51.4399464,-2.5764452,16z/data=!4m5!3m4!1s0x0:0xbe48b80e98e3d84c!8m2!3d51.4404747!4d-2.5740419">
-<img src = " <?php the_field('map'); ?> ">
-</a>
+
+<div class=" lightgreen">
+
+    <h2 class="fp-graph">Location</h2>
+    <div class="flex">
+        
+        <div class="fp-box">
+
+            <div class="btm-margin">
+                <p class=" h2">Email:</p>
+                <a  href="<?php the_field('email_address', 'option'); ?>" > <?php the_field('email_address', 'option'); ?></a> 
+            </div>
+
+            <div class="btm-margin">
+                <p class=" h2">Mobile:</p>
+                <a  href="tel:<?php the_field('mobile_number', 'option'); ?>"> <?php the_field('mobile_number', 'option');  ?></a>
+           
+            </div>
+                <p class=" h2">Landline:</p>
+                <div class="btm-margin">
+                <a href="tel:<?php the_field('landline_number', 'option'); ?>"> <?php the_field('landline_number', 'option');  ?></a>
+            </div>
+
+        </div>
+    <a  href="https://www.google.com/maps/place/Totterdown+Children's+Community+Workshop/@51.4399464,-2.5764452,16z/data=!4m5!3m4!1s0x0:0xbe48b80e98e3d84c!8m2!3d51.4404747!4d-2.5740419">
+    <img class="img-fluid container-sm btm-margin" src = " <?php the_field('map'); ?> ">        </a>
+    </div>   
 </div>
 
     
-    <div>
-
-        <h1>
-            Clubs
-        </h1>
+<div>
+    <h1 class="h2 fp-graph">Clubs</h1>
+<div class="flex">
+    <div class="fp-box">
         
         <h2>Afterschool</h2>
         
         <p>  <?php the_field('after_school_description'); ?> </p>
         <h2>Holiday</h2>
         <p>  <?php the_field('holiday_club_description'); ?> </p>
+    </div>
+
+</div>       
 <?php
        $link = get_field('ipal_link');
 if( $link ): ?>
-    <button class="btn btn-outline-success" href="<?php echo esc_url( $link ); ?>">Book For Holiday Club</button>
+    <button class="btn btn-outline-success pad" href="<?php echo esc_url( $link ); ?>">Book For Holiday Club</button>
 <?php endif; ?>
 
 
