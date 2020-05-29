b2d268edfce9579a7be102ee1327c546728039e4
Jim Ross <jimrossuk@gmail.com>
2020-05-29 16:10:33 +0100

    added border line on footer

--- footer.php
+++ footer.php
@@ -1,39 +1,41 @@
 		<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-<div class="footer-style">
+		<div class="y">
 
-	<div id="inner-footer" class="wrapp cf">
-		
-		<nav role="navigation">
-			<?php wp_nav_menu(array(
-				'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
-				'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
-				'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-				'menu_class' => 'nav footer-nav cf',            // adding custom nav class
-				'theme_location' => 'footer-links',             // where it's located in the theme
-				'before' => '',                                 // before the menu
-				'after' => '',                                  // after the menu
-				'link_before' => '',                            // before each link
-				'link_after' => '',                             // after each link
-				'depth' => 0,                                   // limit the depth of the nav
-				'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
-			)); ?>
-					</nav>
+			<div class="footer-style">
 				
+				<div id="inner-footer" class="wrapp cf">
+					
+					<nav role="navigation">
+						<?php wp_nav_menu(array(
+							'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
+							'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+							'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
+							'menu_class' => 'nav footer-nav cf',            // adding custom nav class
+							'theme_location' => 'footer-links',             // where it's located in the theme
+							'before' => '',                                 // before the menu
+							'after' => '',                                  // after the menu
+							'link_before' => '',                            // before each link
+							'link_after' => '',                             // after each link
+							'depth' => 0,                                   // limit the depth of the nav
+							'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
+						)); ?>
+					</nav>
+					
+					
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					
+					
+					<div>
 						
-						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
-						
-						
-						<div>
-							
-							<?php 
+						<?php 
 
-								$link = get_field('ofsted' ,'options');
-								if( $link ): ?>
+$link = get_field('ofsted' ,'options');
+if( $link ): ?>
 								<a class="footer-style xxx" href="<?php echo esc_url( $link ); ?>">Ofsted report</a>
-							<?php endif; ?>
-							
-							<div>
-								<?php 
+								<?php endif; ?>
+								
+								<div>
+									<?php 
 								$link = get_field('charity_link','options');
 								if( $link ): ?>
 									<a class="footer-style" href="<?php echo esc_url( $link ); ?>">Charity information</a>
@@ -47,9 +49,10 @@
 							</div>
 							
 						</div>
-</div>
-					</footer>
-					
+					</div>
+				</div>
+				</footer>
+				
 					
 
 		<?php // all js scripts are loaded in library/bones.php ?>
