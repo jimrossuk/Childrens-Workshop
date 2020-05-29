ff49108b1800ba0c367be2e96dcf0632075f7248
Jim Ross <jimrossuk@gmail.com>
2020-05-29 17:36:45 +0100

    added border top to Staff name and made photos circles via Bootstrap

--- page-staff.php
+++ page-staff.php
@@ -4,37 +4,44 @@ get_header();
 
 // check if the repeater field has rows of data
 if( have_rows('all_staff') ):
-
- 	// loop through the rows of data
+    
+    // loop through the rows of data
     while ( have_rows('all_staff') ) : the_row();
-
-        // display a sub field value
-        ?>
-       <h1 class="staff_name">  <?php the_sub_field('staff_member_name'); ?> </h1>
+    
+    // display a sub field value
+    ?>
+    
+<div class="x">
+<?php
+?>
+       <h1 class="staff_name ">  <?php the_sub_field('staff_member_name'); ?> </h1>
        <h3 class= "job_titles ">   <?php the_sub_field('job_title'); ?>  
-       </h3>
-
-       <div class="flex-container"> 
-          
-
-               <img class= "flex-item rounded" src= "<?php the_sub_field('staff_photo'); ?>" >
-               
-               <p class="info-container"> <?php the_sub_field('profile');?> </p>
-           
-      </div> 
-       <?php
+    </h3>
+    
+    <div class="flex-container"> 
+        
+        
+        <img class= "flex-item rounded-circle" src= "<?php the_sub_field('staff_photo'); ?>" >
+        
+        <p class="info-container "> <?php the_sub_field('profile');?> </p>
+        
+    </div> 
+    <?php
             
-    endwhile;
-
-else :
-
-    // no rows found
-    
-    
-
-endif;
-
-
+    ?>
+    </div>
+    <?php
+        endwhile;
+        
+        else :
+            
+            // no rows found
+            
+            
+            
+        endif;
+        
+       
 get_footer();
 
 
